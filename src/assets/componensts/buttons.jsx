5c97a13111617860dfee302e52ea8614d77d6ab7
nicolamarino <nicolamarino527@gmail.com>
2025-02-17 22:43:26 +0100

    [fix] fix function

--- src/assets/componensts/buttons.jsx
+++ src/assets/componensts/buttons.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
-import languages from "../languages"
+import languages from "../languages";
 
 
 export default function Buttons() {
     // impostiamo lo stato per il titolo selezionatio
-    const [selectedTitle, selectedDescription] = useState(languages[0])
+    const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
 
     // creiamo la funzione per gestire il click sul bottone
-    const buttonClick = (lenguage) => {
-        setDescription(lenguage)
+    const buttonClick = (language) => {
+        setSelectedLanguage(language);
     }
 
     return (
 
         <div>
-            test
+            <div>
+                {languages.map((language) => (
+                    <button key={language.id} type="button" className="btn btn-primary m-2" onClick={() => buttonClick(language)}>
+                        {language.title}
+                    </button>
+                ))}
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <p>{selectedLanguage.description}</p>
+                </div>
+            </div>
         </div>
     );
 }
